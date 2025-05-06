2d33fcbf8694307d42fd8defc61e67ab80527c86
deeruniverse <igwangi22@gmail.com>
2025-05-06 13:19:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function addToCart(productName) {
+function addToCart(productName, productPrice) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const index = cart.findIndex(item => item.name === productName);
+  if (index > -1) {
+    cart[index].qty += 1;
+  } else {
+    cart.push({ name: productName, price: productPrice, qty: 1 });
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
   alert(productName + " 장바구니에 담김!");
 }
